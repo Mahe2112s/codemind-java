33762913ecfea33ceb2226b2a9b6c59f236e257d
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-09-23 11:30:26 +0530

    import java.util.*; class small {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             int l=0;             for(int j=0;j<n;j++)             if(x[i]>x[j])             {                 l++;             }             System.out.print(l+" ");         }     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class small
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            int l=0;
+            for(int j=0;j<n;j++)
+            if(x[i]>x[j])
+            {
+                l++;
+            }
+            System.out.print(l+" ");
+        }
+    }
+}
