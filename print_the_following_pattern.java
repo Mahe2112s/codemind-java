7538598a8b0b41a12dbf576411a7201ab8fa7c8f
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-10-27 11:07:12 +0530

    import java.util.*; class Pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i,j;         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 System.out.print((char)(64+i)+" ");             }             System.out.println();         }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,25 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 class Pattern
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
+        int n=sc.nextInt();
         int i,j;
-        for(i=1;i<=a;i++)
+        for(i=1;i<=n;i++)
         {
-            for(j=1;j<=a;j++)
+            for(j=1;j<=n;j++)
             {
-                if(i==j || j==1 || (i+j)==(a+j))
-                {
-                    System.out.print("*");
-                }
-                else
-                {
-                    System.out.print(" ");
-                }
+                System.out.print((char)(64+i)+" ");
             }
             System.out.println();
         }
+        
     }
 }
