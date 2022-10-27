1bf47a8e774eed03bb2dcd810840d586ff5c6545
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-10-27 11:19:01 +0530

    import java.util.*; class Pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i,j;         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 System.out.print(i+" ");             }             System.out.println();         }
        }
    }

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Pattern
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
