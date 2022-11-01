bfd9e0f461920aa6be83e233650db0fcbe1e1d30
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-11-01 20:15:56 +0530

    import java.util.*; class Gcd {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         while(a!=b)         {             if(a>b)             {                 a=a-b;             }             else             {                 b=b-a;             }         }         System.out.println(a);     } }

---
+++ GCD_or_HCF.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Gcd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        while(a!=b)
+        {
+            if(a>b)
+            {
+                a=a-b;
+            }
+            else
+            {
+                b=b-a;
+            }
+        }
+        System.out.println(a);
+    }
+}
